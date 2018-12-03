a98d4013a43f0bcf0b0ab5e40b88ffec08f96e86
haythem33 <42939839+haythem33@users.noreply.github.com>
2018-12-03 13:03:51 +0100

    login front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+import { from } from 'rxjs';
+import { Route, Router } from '@angular/router';
+import { ApiService } from '../api.service';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  email;
+  password;
+  message;
+  imageUser: File = null;
+  constructor(  public router: Router, public apiService: ApiService ) {  }
+  ngOnInit() {
+  }
+  loginBtn() {
+    const myObj = { email: this.email, password: this.password };
+    this.message = '';
+    this.apiService.loginApi(myObj).subscribe(res => {
+      console.log(res);
+       if (res.json().message === 'OK') {
+         this.router.navigateByUrl('/home');
+       } else {
+         this.message = res.json().message;
+         console.log(this.message);
+       }
+    });
+  }
+}
